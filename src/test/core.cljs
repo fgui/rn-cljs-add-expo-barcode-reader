2f417a350700b5966d5049a7d2ded01a9e1037e6
francesc <francesc.guillen@gmail.com>
2021-08-04 15:06:19 +0200

    Add expo barcode reader to plain react native.

--- src/test/core.cljs
+++ src/test/core.cljs
@@ -1,9 +1,25 @@
 (ns test.core
-  (:require [steroid.rn.core :as rn]))
+  (:require [steroid.rn.core :as rn]
+            [reagent.core :as reagent]
+            ["expo-barcode-scanner" :as ebs])
+  )
+
+
+(def bar-code-scanner (reagent/adapt-react-class ebs/BarCodeScanner))
 
 (defn root-comp []
-  [rn/view
-   [rn/text "Hello CLojure! from CLJS"]])
+  [rn/view {:style {:flex 1 :flex-direction :column :justify-content :center}}
+   ;;[rn/text "Hello CLojure! from CLJS"]
+   [bar-code-scanner
+    {
+     :style {
+             :flex 1,
+             :justify-content :flex-end
+             :align-items :center}
+     :barcode-types [ebs/BarCodeScanner.Constants.BarCodeType.code128]
+     :on-bar-code-scanned #(js/alert (.-data %))} 
+    ]
+   ])
 
 (defn init []
   (rn/register-reload-comp "Test" root-comp))
